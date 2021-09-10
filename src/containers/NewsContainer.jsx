7ffec005887c7742e49315ed24a31ba5769a4e9a
Kyle Mayer <x@kylemayer.dev>
2021-09-10 11:16:55 -0700

    news container updated

--- src/containers/NewsContainer.jsx
+++ src/containers/NewsContainer.jsx
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-
+import Controls from '../components/news/Controls';
+import NewsList from '../components/news/NewsList';
 export default class NewsContainer extends Component {
   state = {
     loading: true,
-    quotes: [],
-    characterName: '',
+    news: [],
+    newsTitle: '',
   };
 
-
   render() {
+
+    const { news, newsTitle } = this.state;
+
     return (
       <>
-
+        <Controls
+          newsTitle={newsTitle}
+          onTitleChange={this.handleTitleChange}
+          onSubmit={this.handleSubmit}
+        />
+        <NewsList news={news} />
       </>
     );
   }
