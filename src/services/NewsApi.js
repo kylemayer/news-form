13e7e22bc4459258e499fd6bbb18aafe4a1a4863
Kyle Mayer <x@kylemayer.dev>
2021-09-11 12:02:37 -0700

    fixed map on service file

--- src/services/NewsApi.js
+++ src/services/NewsApi.js
@@ -1,13 +1,13 @@
 /* eslint-disable max-len */
 export const formatArticles = (json) =>
-  json.map((article) => ({
+  json.articles.map((article) => ({
     title: article.title,
     author: article.author,
     description: article.description,
   }));
 
 export const fetchArticles = async () => {
-  const response = await fetch('https://newsapi.org/v2/everything?q=bitcoin&apiKey=a5bb6384dbe143e49ab13e067894a099');
+  const response = await fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=a5bb6384dbe143e49ab13e067894a099');
   const json = await response.json();
 
   return formatArticles(json);
